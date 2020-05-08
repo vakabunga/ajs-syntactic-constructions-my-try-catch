eb08f5e5c326eba173306e20af8745e4d6cbae88
Vakabunga <shesterov@gmail.com>
2020-05-09 01:38:19 +0300

    add entry point to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
   },
